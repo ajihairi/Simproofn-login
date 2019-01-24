899d69f40f84584010b7b32952e2fce2f7bf14a8
Hamzhya Tinnov <hamzhyatinnov@gmail.com>
2019-01-24 17:37:45 +0700

    on going implement message list

---
+++ Simproofn/Interfaces/HeaderView.swift
@@ -0,0 +1,52 @@
+//
+//  headerView.swift
+//  Simproofn
+//
+//  Created by Hamzhya on 24/01/19.
+//  Copyright © 2019 Hamzhya. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import UIKit
+
+@IBDesignable
+class HeaderView: UINavigationBar {
+    
+    @IBInspectable var firstColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var secondColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var isHorizontal: Bool = true {
+        didSet {
+            updateView()
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        get {
+            return CAGradientLayer.self
+        }
+    }
+    
+    func updateView() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [firstColor, secondColor].map {$0.cgColor}
+        if (isHorizontal) {
+            layer.startPoint = CGPoint(x: 0, y: 0.5)
+            layer.endPoint = CGPoint (x: 1, y: 0.5)
+        } else {
+            layer.startPoint = CGPoint(x: 0.5, y: 0)
+            layer.endPoint = CGPoint (x: 0.5, y: 1)
+        }
+    }
+    
+}
